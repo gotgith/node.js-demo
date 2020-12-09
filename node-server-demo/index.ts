3aa61605a854d3a8c0e79e4532bbb77577e047c2
JH <18468107890@163.com>
2020-12-09 15:31:44 +0800

    测试server的监听和数据接收

--- node-server-demo/index.ts
+++ node-server-demo/index.ts
@@ -1,1 +1,22 @@
-console.log('hi')
+import * as http from 'http';
+import {IncomingMessage, ServerResponse} from 'http';
+
+const server = http.createServer();
+server.on('request', (request: IncomingMessage, response: ServerResponse) => {
+  console.log(request.method);
+  console.log(request.url);
+  console.log(request.headers);
+  const array = [];
+  // 接收数据，因为数据传输有大小限制，分成多个包上传，因此需要用数组接收
+  request.on('data', (chunk) => {
+    array.push(chunk);
+  });
+  // 数据接收完成，把数据进行拼接
+  request.on('end', () => {
+    const body = Buffer.concat(array).toString();
+    console.log('body');
+    console.log(body);
+    response.end('hi');
+  });
+});
+server.listen(8888);
