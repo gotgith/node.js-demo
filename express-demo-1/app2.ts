d84aa245986ee3faa7fdf960f1bf13aa0b925da5
JH <18468107890@163.com>
2020-12-16 11:52:24 +0800

    express初始化项目配置

---
+++ express-demo-1/app2.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const port = 4000;
+
+app.post('/', (request, response, next) => {});
+app.get('/', (req, res) => {
+  res.send('你好!');
+});
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
